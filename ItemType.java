0b2d421d2ee9f8424cce4895367a3896a6128cd3
Bryson Bennett <blb64218@uga.edu>
2023-01-19 19:16:33 -0500

    ItemType.java setup

--- ItemType.java
+++ ItemType.java
@@ -1,1 +1,19 @@
+public class ItemType {
 
+    private int value;
+
+    int compareTo(ItemType item) {
+
+
+    } // compareTo
+
+    int getValue( ) {
+
+
+    } //getValue
+
+    void initialize(int num) {
+
+
+    } // initialize
+} // ItemType
