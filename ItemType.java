d66248e6b1bd833da96d83e62fe931f59cfb6438
Bryson Bennett <blb64218@uga.edu>
2023-01-20 10:51:31 -0500

    ItemType and SortedLinkedList javadoc comments

--- ItemType.java
+++ ItemType.java
@@ -2,18 +2,32 @@ public class ItemType {
 
     private int value;
 
+    /**
+     * Compares the value of item with the current object's value.
+     * @param item the item to be compared with.
+     */
     int compareTo(ItemType item) {
-
-
+        if(item.getValue() > this.value) {
+            return -1;
+        } else if(item.getValue() == this.value) {
+            return 0;
+        } else {
+            return 1;
+        } // if
     } // compareTo
 
-    int getValue( ) {
-
-
+    /**
+     * Returns the value of instance variable.
+     */
+    int getValue() {
+        return this.value;
     } //getValue
 
+    /**
+     * Initializes the data member by variable num.
+     * @param num The int val is initialized to.
+     */
     void initialize(int num) {
-
-
+        this.value=num;
     } // initialize
 } // ItemType
