e5105e1c52c86465cd1395c3585a9ec3a317521d
Bryson Bennett <blb64218@uga.edu>
2023-01-24 21:46:12 -0500

    started LinkedListDriver

--- src/LinkedListDriver.java
+++ src/LinkedListDriver.java
@@ -1,1 +1,52 @@
+import java.util.Scanner;  // Import the Scanner class
 
+  public class LinkedListDriver {
+
+      public static void main(string[]args) {
+
+          boolean quit = false;
+          String input;
+          Scanner scan = new Scanner(System.in);
+
+         Scanner getList = new Scanner(System.in);
+         ItemType item = new ItemType();
+         SortedLinkedList list = new SortedLinkedList();
+
+         while (getList.hasNext()) {
+             item.initialize(getList.next());
+             list.insertItem(item);
+         } // while
+
+         getList.close();
+
+         while(quit == false) {
+             System.out.println("Enter a command: ");
+             input = scan.nextLine();
+
+             if(input.equals("q")) {
+                 quit = true;
+             } else if(input.equals("l")) {
+                 System.out.println(list.getLength());
+             } else if(input.equals("p")) {
+
+             } else if(input.equals("t")) {
+			 } else if(input.equals("m")) {
+
+             } else if(input.equals("a")) {
+
+             } else if(input.equals("r")) {
+
+             } else if(input.equals("n")) {
+
+             } else if(input.equals("s")) {
+                 System.out.println("Enter a number to search: ");
+             } else if(input.equals("d")) {
+                 System.out.println("Enter a number to delete: ");
+             } else if(input.equals("i")) {
+                 System.out.println("Enter a number to insert: ");
+             } else {
+
+             } // if
+			} // while
+		} // main
+	} // LinkedListDriver
