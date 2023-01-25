dd913013533eced2fe2f1dec66a820e5773f26ed
Michael Le <ml10674@uga.edu>
2023-01-24 21:08:53 -0500

    checking

---
+++ src/SortedLinkedList.java
@@ -0,0 +1,58 @@
+public class SortedLinkedList {
+
+    private NodeType head;
+    private NodeType currentPos;
+
+    /**
+     * Initializes a sorted linked list object.
+     */
+    public SortedLinkedList() {
+
+    } // SortedLinkedList
+
+    /**
+     * Returns the length of the linked list.
+     */
+    public int getLength() {
+
+    } // getLength
+
+    /**
+     * Inserts an item into the linked list while maintaining the ascending sorted order.
+     * @param item the item to be inserted.
+     */
+    public void insertItem(ItemType item) {
+
+    } // insertItem
+
+    /**
+     * Removes the node in the list that contains an item equal to item.
+     * @param item the item to be deleted.
+     */
+    public void deleteItem(ItemType item) {
+
+    } // deleteItem
+
+    /**
+     * Searchs the linked list for an item and returns its index.
+     * @param item the item to search for.
+     */
+    public int searchItem(ItemType item) {
+
+    } // searchItem
+
+    /**
+     * Returns the next item in the list pointed by the currentPos.
+     */
+    public ItemType getNextItem() {
+
+    } // getNextItem
+
+    /**
+     * Initialize the currentPos variable to null.
+     */
+    public void resetList() {
+
+    } // resetList
+
+} // SortedLinkedList
